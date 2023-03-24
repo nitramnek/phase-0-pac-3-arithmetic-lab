b54ffbc0c0d990309cd7a9977fae5f4733cb418f
Kenneth <nitramnek@gmail.com>
2023-03-24 21:42:43 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
 //k3n5c0d3
 
 function add(a, b){
- return a + b;
-}
-
+    return a + b;
+  }
 function subtract(a, b){
     return a - b;
-}
+  }
 function multiply(a, b){
     return a * b ;
-}
+  }
 function divide(a, b){ 
     return a / b;
-}
+  }
 function increment(n){
     return n + 1;
-}
+  }
 function decrement(n){
     return n - 1;
-}
+  }
 function makeInt(n) {
     const parsed = parseInt(n, 10);
     return isNaN(parsed) ? NaN : parsed;
-}
-  
+  }
 function preserveDecimal(n) {
     const parsed = parseFloat(n);
     return isNaN(parsed) ? NaN : parsed;
-}
+  }
 
 //K3n5c0d3
